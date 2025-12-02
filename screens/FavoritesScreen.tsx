beea8ca558374c7a1d233b59230474bde3c7eac5
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-12-02 09:34:32 +0700

    render favorite Meal

--- screens/FavoritesScreen.tsx
+++ screens/FavoritesScreen.tsx
@@ -1,10 +1,14 @@
-import { Text, View } from "react-native";
+import MealList from "@/components/MealList";
+import { MEALS } from "@/data/dummy-data";
+import { FavoritesContext } from "@/store/context/favorites-context";
+import { useContext } from "react";
 
 function FavoritesScreen(){
+    const favoriteMealCtx = useContext(FavoritesContext);
+    const favoritesMeal = MEALS.filter((meal)=> favoriteMealCtx.ids.includes(meal.id));
+
     return (
-        <View>
-            <Text>Favorites Screen</Text>
-        </View>
+        <MealList items={favoritesMeal}/>
     )
 }
 export default FavoritesScreen;
