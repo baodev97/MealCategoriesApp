cdd75fb00286e3b990bf22bfdb78095692b09f1b
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-12-02 09:35:00 +0700

    when no favorite meals

--- screens/FavoritesScreen.tsx
+++ screens/FavoritesScreen.tsx
@@ -2,13 +2,33 @@ import MealList from "@/components/MealList";
 import { MEALS } from "@/data/dummy-data";
 import { FavoritesContext } from "@/store/context/favorites-context";
 import { useContext } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 function FavoritesScreen(){
     const favoriteMealCtx = useContext(FavoritesContext);
     const favoritesMeal = MEALS.filter((meal)=> favoriteMealCtx.ids.includes(meal.id));
+
+    if(favoritesMeal.length === 0){
+        return <View style={styles.rootContainer}>
+            <Text style={styles.text}>You have no favorite meals yet.</Text>
+        </View>
+    }
 
     return (
         <MealList items={favoritesMeal}/>
     )
 }
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+    rootContainer:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    text:{
+        fontSize:18,
+        fontWeight:'bold',
+        color:'white'
+    }
+})
