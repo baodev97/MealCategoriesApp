b131b0cd0f71bb65659dcff68457e4d54feb5cec
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-12-02 10:12:09 +0700

    create Favorite Sclice

---
+++ store/redux/favorites.ts
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export interface FavoritesState {
+    ids:string[]
+}
+
+const initialState: FavoritesState={
+    ids:[]
+}
+
+const favoritesSlice = createSlice({
+    name:'favorites',
+    initialState,
+    reducers:{
+        addFavorite: (state,action)=>{
+            state.ids.push(action.payload.id);
+        },
+        removefavorite:(state, action)=>{
+            state.ids.slice(state.ids.indexOf(action.payload.id),1);
+        }
+    }
+})
+
+export default favoritesSlice.reducer;
